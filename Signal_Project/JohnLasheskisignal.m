4cd627b1a9f1fb3eefdf8fcce7e788ad2c33914b
JMLjml <johnlasheski@gmail.com>
2014-11-15 10:00:10 -0500

    Signal Project complete

--- Signal_Project/JohnLasheskisignal.m
+++ Signal_Project/JohnLasheskisignal.m
@@ -3,13 +3,18 @@
 % JohnLasheskisignal.m
 % November 09, 2014
 %
+% The project studies 3 signal vectors supplied by the course instructor. These signals
+% are plotted in the time domain and then are again plotted using the FFT calculation.
+% Plots are supplied for the regular FFT function as well as using a Welch calculation that
+% uses a hanning window with an overlap of 50%. The Welch calculations break the signal
+% down into three equal parts and plot the pieces separately.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear all;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% Define some of the test data up here
+% Define some of the test data and utility functions up here
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -42,6 +47,19 @@ n3=[0:Ts:((L3/fs) - (1/fs))];
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% function signal = strip_DC(signal)
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Usage: 
+%        strip_DC(signal)
+%
+%        Subtract the mean of the signal from itself, thereby removing
+%        the DC component from the signal
+%        
+%        Author: John Lasheski
+%        Date: November 12, 2014
 function signal = strip_DC(signal)
     DC_Component = mean(signal);
     signal = signal - DC_Component;
@@ -50,12 +68,35 @@ end
 
 
 
-function db = pow2db(signal)
-    db = 10 * log10(signal);
-end
-
-
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% function plotWelch(signal, signal_length, N, fs, figure_count, fftRange, welchRange)
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Usage: 
+%        plotWelch(signal, signal_length, N, fs, figure_count, fftRange, welchRange)
+%
+%        plotWelch computes the regular FFT a,d Welch FFT on signal. It divides the signal into
+%        three equal parts for plotting the Welch FFT. The Welch FFT uses a hanning window with
+%        50% overlap. plotWelch plots an FFT graph and 3 Welch FFT in a single Figure denoted 
+%        by figure count.
+%
+%        signal: a vector representing the signal under study
+%
+%        signal_length: integer determining how long the signal is. Used to override the true vector length.
+%
+%        N: number of points in 1 second of signal data
+%
+%        fs: sample frequency of the signal
+%
+%        figure_count: used to denote which figure octave should plot the graphs on
+%
+%        fftRange: used to control the x axis range for the FFT plot
+%
+%        welchRange: used to control the x axis range for the Welch FFT plots
+%        
+%        Author: John Lasheski
+%        Date: November 12, 2014
 function plotWelch(signal, signal_length, N, fs, figure_count, fftRange, welchRange)
 
     % Produce a Hanning Window of the correct length
@@ -75,6 +116,7 @@ function plotWelch(signal, signal_length, N, fs, figure_count, fftRange, welchRa
     viewrange = (1:welchRange);
 
     % plot the regular FFT for comparison purposes
+    label = strcat('FFT Power Graph for Signal =', int2str(figure_count -1));
     subplot(221);
     y = fft(signal, signal_length);
     y0 = fftshift(y);
@@ -84,25 +126,31 @@ function plotWelch(signal, signal_length, N, fs, figure_count, fftRange, welchRa
     set (gca, 'xlim', [-fftRange, fftRange])
     set (gca, 'xlabel', 'Frequency (Hz)', 'fontsize' , 10);
     set (gca, 'ylabel', 'Power Spectral Value', 'fontsize', 10);
+    set (gca, 'title', label);
     grid on;
 
     % plot the 1st second of data using Welch's method
+    label = strcat('1st Interval Welch FFT Graph for Signal =', int2str(figure_count -1));
     subplot(222);
     pxx = pwelch(first, window, 0.5, N, fs, 'linear');
     plot(viewrange, pxx(viewrange), 'color', 'm', 'linewidth', 2);
     set (gca, 'xlabel', 'Frequency (Hz)', 'fontsize' , 10);
     set (gca, 'ylabel', 'Power Spectral Value', 'fontsize', 10);
+    set (gca, 'title', label);
     grid on;
 
     % plot the 2nd second of data using Welch's method
+    label = strcat('2nd Interval Welch FFT Graph for Signal =', int2str(figure_count -1));
     subplot(223);
     pxx = pwelch(second, window, 0.5, N, fs, 'linear');
     plot(viewrange, pxx(viewrange), 'color', 'y', 'linewidth', 2);
     set (gca, 'xlabel', 'Frequency (Hz)', 'fontsize' , 10);
     set (gca, 'ylabel', 'Power Spectral Value', 'fontsize', 10);
+    set (gca, 'title', label);
     grid on;
 
     % plot the 3rd second of data using Welch's method
+    label = strcat('3rd Interval Welch FFT Graph for Signal =', int2str(figure_count -1));
     subplot(224);
     pxx = pwelch(third, window, 0.5, N, fs, 'linear');
     plot(viewrange, pxx(viewrange), 'color', 'g', 'linewidth', 2);
@@ -113,12 +161,13 @@ function plotWelch(signal, signal_length, N, fs, figure_count, fftRange, welchRa
 end
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
-% Build the variuos plots for the assignment
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Build the various plots for the assignment
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % FIGURE 1: Plot the input data from the Signal files
