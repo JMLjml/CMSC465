5c22050be97c8687c1023c49f696d5fd011fda82
JMLjml <johnlasheski@gmail.com>
2014-11-24 13:18:05 -0500

    Added Edge Detection Methods. Ready for testing.

--- EdgeDetection.m
+++ EdgeDetection.m
@@ -3,15 +3,10 @@
 % EdgeDetection.m
 % November 24, 2014
 %
-% Octave script file that explores the nearest, bilinear and bicubic methods of 
-% translating, rotating, and shearing an image. sample image is explored and 
-% plots are created to view the differences in the methods.
+% Octave script file that explores the Sobel, Prewitt, Roberts and Kirsch methods of 
+% edge detection on an image. sample image is explored and plots are created to view the 
+% differences in the methods.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% Use Octave's edge methods to perform edge detection on an image of your choice (bw = edge (im, method, arg1, arg2). 
-% Show the original image, your Octave code, and the edge extraction results for at least 4 different algorithms of 
-% your choice. Which one seems to perform the best on your image? Compare and contrast the edge detection results 
-% indicating strengths and weaknesses you observed.
 
 clear all;
 
@@ -21,3 +16,33 @@ pkg load image;
 % load our test image into octave
 test_image = imread("owl.jpg");
 
+% perform edge detection using four diferrent techniques
+bw_sobel = edge(test_image, 'sobel');
+bw_prewitt = edge(test_image, 'prewitt');
+bw_roberts = edge(test_image, 'roberts');
+bw_kirsch = edge(test_image, 'kirsch');
+
+% display the results
+figure(1);
+hold on;
+
+subplot(151);
+imshow(test_image);
+set(gca, 'title', 'Original Image');
+
+subplot(152);
+imshow(bw_sobel);
+set(gca, 'title', 'Edge Detection using Sobel Method');
+
+subplot(153);
+imshow(bw_prewitt);
+set(gca, 'title', 'Edge Detection using Prewitt Method');
+
+subplot(154);
+imshow(bw_roberts);
+set(gca, 'title', 'Edge Detection using Roberts Method');
+
+subplot(155);
+imshow(bw_kirsch);
+set(gca, 'title', 'Edge Detection using Kirsch Method');
+hold off;
